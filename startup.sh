5d56f38f2caf1e0bf7ea9e292ace875f80e01a1c
aaron-dm-mcdonald <mcdonald.dm.aaron@gmail.com>
2025-08-02 17:03:26 -0700

    fix compose yaml

--- startup.sh
+++ startup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+
+
+dnf install -y docker git
+curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+systemctl enable --now docker
+
 mkdir splunk && cd splunk
-
-# non-prod install script
-curl -fsSL https://get.docker.com -o get-docker.sh
-chmod +x get-docker.sh
-./get-docker.sh
-
 git clone https://github.com/aaron-dm-mcdonald/splunk-stack-test.git .
 
 # get images first
